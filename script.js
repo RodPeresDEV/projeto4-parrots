2e1856d4f8e03d748e4560b4239608f1ac1a615f
Rodrigo Peres <rperes.batista@gmail.com>
2023-01-15 01:46:06 -0300

    versão com tabuleiro

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-let cartas = 0;
-while ((cartas < 4) || (cartas > 14) && (cartas % 2 == 0)) {
-    let quantidade = prompt("Com quantas cartas você quer jogar?");
-    cartas = Number(quantidade);
+const imagens =['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
+
+let quantidadeCartas = 0;
+
+function iniciarJogo() {
+
+    while ((quantidadeCartas < 4) || (quantidadeCartas > 14) || (quantidadeCartas % 2 !== 0)) {
+        quantidadeCartas = Number(prompt("Com quantas cartas você quer jogar?"));
+        
+    }
+    gerarCartas(); 
 }
 
-//function embaralharCartas(){
-//    cartasDoJogo.forEach(card => {
-//        let ramdomPos = Math.floor(Math.random() * quantiaDeCartas);
-//        card.style.order = ramdomPos;
-//    });
-//}
-//embaralharCartas()
+iniciarJogo();
+
+function randomizar() {
+    return Math.random() - 0.5
+}
+
+function gerarCartas() {
+    const tabuleiro = document.querySelector('.tabuleiro')
+
+    const baralho = [];
+    for (let i=0; i < (quantidadeCartas / 2); i++) {
+        const carta = `
+            <div onclick="virarCarta()" class="carta">
+                <img class="frente" src="imgs/back.png" />
+                <img class="verso" src='imgs/${imagens[i]}.gif'
+            </div>
+        `
+        baralho.push(carta)
+        baralho.push(carta)
+    }
+    
+    baralho.sort(randomizar)
+
+    for (let i=0; i < baralho.length; i++) {
+        tabuleiro.innerHTML += baralho[i];
+    } 
+}    
+
+function virarCarta() {
+
+}
